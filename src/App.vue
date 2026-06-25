153a51fd8b756f0af40ab4454fad46ef56f00d85
Loyd-894562 <catalanjhonlyodm@gmail.com>
2026-06-25 10:09:40 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import SampleComponent from './components/SampleComponent.vue'
+</script>
+
+<template>
+  <HelloWorld />
+  <!-- <SampleComponent /> -->
+</template>
---
+++ src/components/SampleComponent.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+    <div>
+        <h2>This is a sample Component.</h2>
+    </div>
+</template>
